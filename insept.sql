aea16932fbe4d0007f64add2be58a9fd8e25e170
karlakraljik <152919944+karlakraljik@users.noreply.github.com>
2023-12-14 20:58:21 +0100

    unos podataka u tablicu smjer

---
+++ insept.sql
@@ -0,0 +1,13 @@
+﻿use edunovawp3;
+
+select *from smjerovi;
+
+insert into smjerovi (naziv,trajanje,cijena,vaucer)
+values ('web programiranje',225, 1859.45,1);
+
+-- losa sintaksa
+insert into smjerovi values
+('web dizajn',null,null,null);
+
+-- minimalna dobra sintaksa
+insert into smjerovi(naziv) values ('ćžčć ĆĐČĆĆ');
