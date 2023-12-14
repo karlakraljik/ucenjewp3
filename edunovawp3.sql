ea63c034b43ba3a40c1e62a9dc3bdc3866347405
karlakraljik <152919944+karlakraljik@users.noreply.github.com>
2023-12-14 20:04:07 +0100

    kreiranje tablica i veza izmedu tablica

---
+++ edunovawp3.sql
@@ -0,0 +1,46 @@
+--create database edunovawp3;
+--ovo je komentar
+--drop database edunovawp3;
+--use master
+--use edunovawp3;
+--create table smjerovi(
+--sifra int,
+--naziv varchar(50),
+--trajanje int,
+--cijena decimal(18,2),
+--vaucer bit
+--);
+--create table grupe(
+--	sifra			int,
+--	naziv			varchar(5),
+--	smjer			int,
+--	datumpocetka	datetime,
+--	maxpolaznika	int,
+--	predavac		int,
+
+--);
+----create table polaznici(
+----sifra int,
+----ime varchar(50),
+----prezime varchar(50),
+----email varchar(100),
+----oib char(11),
+----brojugovora varchar(10)
+
+--);
+--create table predavaci (
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--email varchar(100),
+--oib char(11),
+--iban varchar(50)
+--);
+create table clanovi(
+grupa int,
+polaznik int
+);
+
+
+
+
