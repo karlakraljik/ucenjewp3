2577e9930bd977e328e3a35bfdc03d9c131c56a3
Karla <kraljikkarla@gmail.com>
2024-01-27 18:02:48 +0100

    ulazizlaz

--- CSHARP/UcenjeWP3/UcenjeCS/Program.cs
+++ CSHARP/UcenjeWP3/UcenjeCS/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string ime;
+Console.WriteLine("Unesi ime: ");
+
+ime = Console.ReadLine();
+
+Console.WriteLine(ime);
+
---
+++ CSHARP/UcenjeWP3/WebApi/Controllers/E01ulazizlaz.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController ]
+    [Route ("E01")]
+    public class E01ulazizlaz: ControllerBase
+    {
+        [HttpGet]
+    [Route ("Hello")]
+   
+        public string Hellowold( string ime)
+    {
+        return "Dobro vecer" + ime;
+
+
+
+    }
+
+
+
+
+
+    }
+}
