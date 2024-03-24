7cb916e5659f6ae88c8be2c5baf7e56037646cca
Karla <kraljikkarla@gmail.com>
2024-03-24 11:39:00 +0100

    uvod frontend

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import mojLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import NavBar from './components/NavBar'
+import { Route, Routes } from 'react-router-dom'
+import { RoutesNames } from './constants'
+import Pocetna from './pages/Pocetna'
+import Smjerovi from './pages/smjerovi/Smjerovi'
+
+function App() {
+
+
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path={RoutesNames.HOME} element={<Pocetna />} />
+
+        <Route path={RoutesNames.SMJER_PREGLED} element={<Smjerovi />} />
+        
+      </Routes>
+    </>
+  )
+}
+
+export default App
